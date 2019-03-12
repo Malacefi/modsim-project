5fedc477b2bd5ca82732635059dd0a7761fc4f18
Niklas Ingemar Bergdahl <meradana@hotmail.se>
2019-03-12 11:43:06 +0100

    Add rotation

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -6,6 +6,7 @@ public class SpaceshipController : MonoBehaviour
 {
 
 	private Rigidbody2D body;
+    private float rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,30 @@ public class SpaceshipController : MonoBehaviour
     void FixedUpdate(){
     	float moveHorizontal = Input.GetAxis("Horizontal");
     	float moveVertical = Input.GetAxis("Vertical");
-    	Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    	Vector2 movement = new Vector2(0, moveVertical);
+
+        if(moveHorizontal>0)
+        {
+            rotation += (float)0.2;
+        }
+        if(moveHorizontal<0)
+        {
+            rotation -= (float)0.2;
+        }
+        transform.Rotate(0, 0, rotation, Space.Self);
+        /*
+        if (moveHorizontal>0)
+        {
+            transform.Rotate(0, 0, 1, Space.Self);
+        }
+        if(moveHorizontal<0)
+        {
+            transform.Rotate(0, 0, -1, Space.Self);
+        }*/
+        //transform.Rotate(0, 0, 1, Space.Self);
+
+
+
     	body.AddForce(movement);
     }
 
