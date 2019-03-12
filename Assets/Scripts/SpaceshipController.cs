5439c720a06756b390a4e78e52f3b82d70c2776a
Malacefi <emelie.eriksson@si7.se>
2019-03-12 11:10:13 +0100

    Add assets

---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+
+	private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	body = GetComponent<Rigidbody2D>();
+    }
+
+    // Physics update
+    void FixedUpdate(){
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	float moveVertical = Input.GetAxis("Vertical");
+    	Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    	body.AddForce(movement);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
