52495a212a4edd2f84f47eb5d535522b465d4bd3
Malacefi <emelie.eriksson@si7.se>
2019-03-13 13:31:08 +0100

    Fix so that camera is not rotating

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject spaceship;
+	private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - spaceship.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate(){
+    	transform.position = spaceship.transform.position + offset;
+    }
+}
