4fad99b3fd3ca7082ed1c26fbe3419eefc3a1557
Malacefi <emelie.eriksson@si7.se>
2019-03-15 18:28:55 +0100

    Fix camera field of view

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public GameObject spaceship;
+	public Camera mainCamera;
+
 	private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
     	spaceship = GameObject.Find("Spaceship");
         offset = transform.position - spaceship.transform.position;
+
+        mainCamera = Camera.main;
+        mainCamera.orthographicSize = 7.0f;
     }
 
     // Update is called once per frame
